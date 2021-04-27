153fa7eb47352bf07a7940dd64b5a7765a2767dc
Marine Lasbleis <marine.lasbleis@gmail.com>
2021-04-27 17:41:42 +0200

    version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="coreevolution",
-    version="1.0",
+    version="1.1",
     author="Marine Lasbleis, Irene Bonati",
     author_email="marine.lasbleis@gmail.com",
     description="Heat and energy budget for core evolution",
